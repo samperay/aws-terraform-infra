481462990cfdebd1a7faf5c34dc10b89a788eb5f
samperay <sunlnx@gmail.com>
2024-12-02 19:28:14 +0530

    renamed custom modulues to modules

--- create_ec2/main.tf
+++ create_ec2/main.tf
@@ -1,17 +1,17 @@
 module "vpc" {
-  source            = "../custom_modules/vpc"
+  source            = "../modules/vpc"
   vpc_cidr          = var.vpc_cidr
   region            = var.region
   public_subnet_az1 = var.public_subnet_az1
 }
 
 module "aws_keyname" {
-  source   = "../custom_modules/key_pair"
+  source   = "../modules/key_pair"
   key_name = "demo"
 }
 
 module "security_group" {
-  source  = "../custom_modules/sg"
+  source  = "../modules/sg"
   sg_name = "allow_network_traffic"
   vpc_id  = module.vpc.vpc_id
 }
@@ -20,7 +20,7 @@ module "security_group" {
 
 module "ec2" {
   depends_on    = [module.vpc]
-  source        = "../custom_modules/ec2"
+  source        = "../modules/ec2"
   instance_type = var.instance_type
   region        = var.region
   subnet_id     = module.vpc.aws_subnet_id
--- create_vpc/main.tf
+++ create_vpc/main.tf
@@ -1,5 +1,5 @@
 module "test_vpc" {
-  source            = "../custom_modules/test-vpc"
+  source            = "../modules/vpc"
   vpc_cidr          = var.vpc_cidr
   region            = var.region
   public_subnet_az1 = var.public_subnet_az1
