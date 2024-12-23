7f1aff7c981e903ecff2abb455b5406857340ffe
samperay <sunlnx@gmail.com>
2024-12-23 19:29:45 +0530

    eks cluster

---
+++ registry_modules/eks/eks-nodegroup-private.tf
@@ -0,0 +1,38 @@
+# provision nodes in the private subnet
+resource "aws_eks_node_group" "eks_nodegroup_private" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "${local.name}-eks_nodegroup_private"
+  node_role_arn   = aws_iam_role.eks_nodegroup_role.arn
+  subnet_ids      = module.vpc.private_subnets
+  version         = var.cluster_version
+  ami_type        = "AL2_x86_64"
+  capacity_type   = "ON_DEMAND"
+  disk_size       = 20
+  instance_types  = ["t3.medium"]
+
+  remote_access {
+    ec2_ssh_key = "demo"
+  }
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.eks-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.eks-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.eks-AmazonEC2ContainerRegistryReadOnly,
+  ]
+
+  tags = {
+    Name = "eks_nodegroup_private"
+  }
+}
